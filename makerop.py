31f72ffaf9c1e1b80512208d34755fea3202c626
Mason <49540886+lvcca@users.noreply.github.com>
2021-11-25 16:56:24 -0500

    Update makerop.py

--- makerop.py
+++ makerop.py
@@ -2,11 +2,12 @@
 #	Purpose: Make easy ropchains from desired instructions
 #
 
+import sys
 import random
 import struct
 
-gadget_files = ['snfs_gadgets.txt', 'csncdav6_gadgets.txt', 'csmtpav6_gadgets.txt', 'csftpav6_gadgets.txt'] #File(s) of Gadgets, written with rp++ output
-desired_file = open('desired_gadgets.txt') #Desired gadgets should match the format in gadget file
+gadget_files = ['csncdav6_gadgets.txt', 'csmtpav6_gadgets.txt', 'snfs_gadgets.txt', 'csftpav6_gadgets.txt'] #File(s) of Gadgets, written with rp++ output
+desired_file = open('desired_gadgets.txt', 'r') #Desired gadgets should match the format in gadget file
 
 #ensure match of desired lines to gadget file
 #Example: 	If gadget desired is 'mov esi, esi '
@@ -34,7 +35,7 @@ def check_bad_chars(addr):
 
 #Populate all_gadgets from usable gadgets in gadget file
 for gadget_file in gadget_files:
-	for line in open(gadget_file).readlines():
+	for line in open(gadget_file, 'r').readlines():
 		if "0x" in line:
 			if not check_bad_chars(line.split(':')[0]):
 				all_gadgets.append(line)
@@ -60,9 +61,10 @@ def find_this_gadget(query):
 			addr, other = gadget.split(':')
 			if len(query.split(' ')) != 1:
 				if query in other:
-					matches.append(gadget.strip())
-					if len(gadget) < smallest_match:
-						smallest_match = len(gadget)
+					if 'call' not in other and 'jmp' not in other and 'and' not in other and 'leave' not in other:
+						matches.append(gadget.strip())
+						if len(gadget) < smallest_match:
+							smallest_match = len(gadget)
 	#if no match found
 	if len(matches) == 0:
 		return "[*] Could not find gadget for %s" % query
@@ -76,26 +78,176 @@ def find_this_gadget(query):
 	#print('[*] Found %d matches for "%s."' % (len(matches), query))
 	return result_matches[random.randint(0, len(result_matches)-1)]
 
-#Build ROP chain
-ropchain = []
-for dg in desired_gadgets:
-	result = find_this_gadget(dg)
-	ropchain.append(result)
 
-#Pound to include comment in string output
-pound = '#'
+def build_ROP():
+	#Build ROP chain
+	ropchain = []
+	#For run as module
+	output = b""
+	error_flag = False
 
-print('ropchain = b"" %s Initialize ropchain' % pound)
-linenumber = 1
-for g in ropchain:
-	if "bypass" in g:
-		print("ropchain += pack('<L', (%s)) %s %s [Line: %d]" % (g.split(' ')[1], pound, g, linenumber))
-	elif "#" in g:
-		print('\n' + g + '\n')
-	elif "Could not find gadget for " not in g:
-		print("ropchain += pack('<L', (%s)) %s %s [Line: %d]" % (g.split(':')[0], pound, g, linenumber))
+	global desired_gadgets
+	for dg in desired_gadgets:
+		result = find_this_gadget(dg)
+		ropchain.append(result)
 
+	#Pound to include comment in string output
+	pound = '#'
+
+	print('ropchain = b"" %s Initialize ropchain' % pound)
+	linenumber = 1
+	for g in ropchain:
+		if "bypass" in g:
+			addr = g.split(' ')[1]
+			print("ropchain += pack('<L', (%s)) %s %s [Line: %d]" % (addr, pound, g, linenumber))
+			try:
+				output += struct.pack('<L', int(addr, 0))
+			except:
+				error_flag = True
+				continue
+
+		elif "#" in g:
+			print('\n' + g + '\n')
+		elif "Could not find gadget for " not in g:
+			addr = g.split(':')[0]
+			print("ropchain += pack('<L', (%s)) %s %s [Line: %d]" % (addr, pound, g, linenumber))
+			try:
+				output += struct.pack('<L', int(addr, 0)) 
+			except:
+				print(type(addr))
+				error_flag = True
+				continue
+		else:
+			print("ropchain += pack('<L', (?)) %s %s [Line: %d]" % (pound, g, linenumber))
+			error_flag = True
+
+		linenumber+=1
+
+	if error_flag == False:
+		return output, True
+	elif error_flag == True:
+		return output, False
+
+
+# --- Find Matches ---
+
+# Write Registers
+# Primary Storage Register
+# Secondary Storage Register
+# Instruction Pointer
+# Incr Instruction Pointer
+# Xchg registers
+
+registers = ['eax', 'ebx', 'ecx', 'edx', 'esi', 'edi']
+write_match = []
+primary_storage_match = []
+secondary_storage_match = []
+instruction_pointer_match = []
+increment_instruction_match = []
+single_pop_match = []
+add_match = []
+sub_match = []
+neg_match = []
+
+def find_reg(search, search_type):
+	global registers
+	match = []
+	good_registers = []
+	for dst_reg in registers:
+		for src_reg in registers:
+			do_search = search % (dst_reg, src_reg)
+			check = find_this_gadget(do_search)
+			if '[*]' not in check:
+				print('[*] %s match -- %s' % (search_type, check.split(':')[1].lstrip().split('(')[0]))
+				match.append(check)
+				good_registers.append([dst_reg, src_reg])
+	return match, good_registers
+
+def find_instruction_pointer():
+	match = []
+	registers = ['esp', 'ebp']
+	good_registers = []
+	for reg in registers:
+		search = 'push %s ; ret' % reg
+		check = find_this_gadget(search)
+		if '[*]' not in check:
+			try:
+				print('[*] Psuedo instruction pointer -- ' + check.split(':')[1].split('(')[0])
+				match.append(check)
+				good_registers.append(reg)
+			except:
+				print(check)
+				continue
+	return match, good_registers
+
+def find_single_pop():
+	match = []
+	global registers
+	for reg in registers:
+		search = "pop %s ; ret" % reg
+		check = find_this_gadget(search)
+		if '[*]' not in check:
+			try:
+				print('[*] Single pop -- ' + check.split(':')[1].split('(')[0])
+				match.append(check)
+			except:
+				print(check)
+				continue
+	return match
+
+def find_inc():
+	global registers
+	match = []
+	for reg in registers:
+		search = 'inc %s ; ret' % reg
+		check = find_this_gadget(search)
+		if '[*]' not in check:
+			print('[*] Inc -- %s' + check.split(':')[1].split('(')[0])
+			match.append(check)
+	return match
+
+def find_reg_single(query, searchtype):
+	match = []
+	global registers
+	for reg in registers:
+		search = query % reg
+		check = find_this_gadget(search)
+		if '[*]' not in check:
+			try:
+				print('[*] %s -- %s' % (searchtype,check.split(':')[1].split('(')[0]))
+				match.append(check)
+			except:
+				print(check)
+				continue
+	return match
+
+def get_matches():
+	#Get matches for the following instructions
+	write_match, write_regs = find_reg('mov dword [%s], %s ; ret', 'Patch')
+	primary_storage_match, storage_regs = find_reg('mov %s, %s ; ', 'Move')
+	instruction_pointer_match, ip_regs = find_instruction_pointer()
+	secondary_storage_match, secondary_regs = find_reg('push %s ; pop %s ', 'Push')
+	single_pop_match = find_single_pop()
+	xchg_match, xchg_regs = find_reg('xchg %s, %s ; ret', 'Xchg')
+	add_match, add_regs = find_reg('add %s, %s ; ret', 'Add')
+	sub_match, sub_regs = find_reg('sub %s, %s ; ret', 'Sub')
+	neg_match, neg_regs = find_reg('neg %s, %s ;', 'Neg')
+	inc_match = find_inc()
+	mov_esp_match = find_reg_single('xor %s, esp', 'XOR')
+	push_matches = find_reg_single('push %s ; ', 'Push')
+	stack_moves = find_reg_single('mov  %s, esp', 'ESP')
+
+if __name__ == '__main__':
+
+	if len(sys.argv) > 1:
+
+		if 'ropchain' in sys.argv[1]:
+			build_ROP()
+		elif 'matches' in sys.argv[1]:
+			get_matches()
 	else:
-		print("ropchain += pack('<L', (?)) %s %s [Line: %d]" % (pound, g, linenumber))
-
-	linenumber+=1
+		print('[*] -- Invalid Console Syntax --')
+		print('[*] To print valid matches set in get_matches() use matches')
+		print('[*] To print ROPchain from gadget_file(s) and desired_gadgets file use ropchain.')
+		print("[*] Usage: python3 makeROP.py \'ropchain\'")
+		print("[*] Usage: python3 makeROP.py \'matches\'")
